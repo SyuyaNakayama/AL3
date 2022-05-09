abafa286c568a52dd294cc9bde07feda5beee292
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2022-05-09 12:31:59 +0900

    ここからのSTART

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,9 +1,6 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-#include <random>
-
-using namespace DirectX;
 
 GameScene::GameScene() {}
 
@@ -15,45 +12,9 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
-	textureHandle_ = TextureManager::Load("mario.jpg");
-	model_ = Model::Create();
-
-	targetNum = 0;
-	vecToNext = {0, 0, 0};
-
-	for (size_t i = 0; i < 3; i++) {
-		worldTransform_[i].translation_ = {
-		  5.0f * cosf(XM_2PI / 3.0f * (float)i + XM_PI / 2.0f),
-		  5.0f * sinf(XM_2PI / 3.0f * (float)i + XM_PI / 2.0f), 0.0f};
-		worldTransform_[i].Initialize();
-	}
-
-	viewProjection_.eye.z = -25;
-	viewProjection_.target = worldTransform_[targetNum].translation_;
-	viewProjection_.Initialize();
 }
 
-void GameScene::Update() {
-	// 何Fで次のオブジェクトを注視するか
-	const int DIV_VEC = 6;
-
-	if (input_->TriggerKey(DIK_SPACE)) {
-		if (++targetNum == 3) {
-			targetNum = 0;
-		}
-		viewProjection_.target = worldTransform_[targetNum].translation_;
-	}
-
-	viewProjection_.UpdateMatrix();
-
-	debugText_->SetPos(50, 50);
-	debugText_->Printf(
-	  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
-	debugText_->SetPos(50, 70);
-	debugText_->Printf(
-	  "target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y,
-	  viewProjection_.target.z);
-}
+void GameScene::Update() {}
 
 void GameScene::Draw() {
 
@@ -81,9 +42,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	for (size_t i = 0; i < 3; i++) {
-		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
-	}
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,24 +1,45 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DebugText.h"
 #include "DirectXCommon.h"
+#include "DebugText.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include <DirectXMath.h>
 
+/// <summary>
 /// ゲームシーン
+/// </summary>
 class GameScene {
-  public:              // メンバ関数
-	GameScene();       // コンストクラタ
-	~GameScene();      // デストラクタ
-	void Initialize(); // 初期化
-	void Update();     // 毎フレーム処理
-	void Draw();       // 描画
+
+  public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
 
   private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
@@ -26,12 +47,7 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
-	uint32_t textureHandle_ = 0;
-	Model* model_ = nullptr;
-	// ゲームシーン用
-	WorldTransform worldTransform_[3];
-	ViewProjection viewProjection_;
-
-	int targetNum;
-	XMFLOAT3 vecToNext;
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
 };
